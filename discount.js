f22e9a0d1971fd8b3f3a2aaf0043b1827581141b
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-07-16 16:33:32 +0600

    add updates all changes and all line codes

---
+++ discount.js
@@ -0,0 +1,40 @@
+// const totalAmount = 4000;
+
+// if (totalAmount > 5000) {
+//     const discount = 30;
+//     const discountAmount = totalAmount / 100 * discount;
+//     const payment = totalAmount - discountAmount;
+//     console.log(payment);
+// }
+// else if (totalAmount > 1000) {
+//     const discount = 10;
+//     const discountAmount = totalAmount / 100 * discount;
+//     const payment = totalAmount - discountAmount;
+//     console.log('Please pay:', payment)
+// }
+
+
+// let age = 15;
+
+// if (age > 18) {
+//     console.log("Adult");
+// }else {
+//     console.log("Minor");
+// }
+
+// let age = 20;
+
+// let result = age >= 18 ? "Adult" : "Minor";
+
+// console.log(result);
+
+
+
+// let age = 22;
+// let hasID = true;
+
+// if (age >= 18) {
+//     if (hasID) {
+//         console.log("Entry Allowed");
+//     }
+// }
